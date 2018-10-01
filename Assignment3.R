b8c9e75b11dee1f57f7b6b2d2f1c7c5dd94be05f
XueweiQuan <43186122+XueweiQuan@users.noreply.github.com>
2018-10-01 14:32:00 -0400

    Update Assignment3.R

--- Assignment3.R
+++ Assignment3.R
@@ -8,7 +8,8 @@ dataplot <- filter(data, year == 2007)
 dataplot%>%
   ggplot(aes(x =gdpPercap, y =lifeExp))+
   geom_point()+
-  geom_text(data = dataplot[dataplot$gdpPercap>40000,], aes(label = country), color = "blue")+
+  geom_text(data = dataplot[dataplot$gdpPercap>40000 & dataplot$continent == "Europe",], 
+            aes(label = country), color = "blue", vjust = 1.5)+
   labs(x = "GDP per capita", y = "life expextancy")+
   theme_bw()
 
@@ -19,6 +20,6 @@ dataplot%>%
   ggplot(aes(x =gdpPercap, y =lifeExp))+
   geom_point(shape = 1, size = 2, stroke = 1.5)+
   annotate("rect", xmin = 39000, xmax = 51000, ymin = 75, ymax = 85, fill = "red", alpha = 0.2)+
-  annotate("text", x = 45000:44998, y = 73:71, label = c("Countries with", "", "highest GDP"), size = 4.5)+
+  annotate("text", x = 45000, y = 72, label = "Countries with \n highest GDP", size = 4.5)+
   labs(x = "GDP per capita", y = "life expextancy")+
   theme_bw()
